847d4c7693cac6dafac9f2cdff80ff2b6df9b32d
raonismaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2019-09-11 14:37:09 -0300

    project structure

---
+++ src/test/java/cloud/fogbow/auditingserver/AuditingServerApplicationTests.java
@@ -0,0 +1,16 @@
+package cloud.fogbow.auditingserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuditingServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
