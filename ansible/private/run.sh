4f83b6581f699f1fecaa69fd73838addf9d273d7
raonismaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2019-10-02 15:50:12 -0300

    filling application.properties automatically

--- ansible/private/run.sh
+++ ansible/private/run.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 cd /home/ubuntu/auditing
+
+host_ip=$(grep ^host_ip general.conf | awk -F "=" '{print $2}')
+db_container_port=$(grep ^container_port general.conf | awk -F "=" '{print $2}')
+db_name=$(grep ^db_name general.conf | awk -F "=" '{print $2}')
+db_url=$host_ip:$db_container_port/$db_name
+db_user=$(grep ^db_user general.conf | awk -F "=" '{print $2}')
+db_password=$(grep ^db_password general.conf | awk -F "=" '{print $2}')
+
+sed -i 's,spring.datasource.url.*,spring.datasource.url=jdbc:postgresql://$db_url,' ./application.properties
+sed -i 's,spring.datasource.username.*,spring.datasource.username=$db_user,' ./application.properties
+sed -i 's,spring.datasource.password.*,spring.datasource.password=$db_password,' ./application.properties
+
 git clone https://github.com/eubr-atmosphere/auditing-server.git
 mv general.conf auditing-server/database-setup
 cd auditing-server/database-setup
