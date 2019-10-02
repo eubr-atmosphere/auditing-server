1f35968c0a3e94dfd4a2cbcad45aff95619aa47a
raonismaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2019-10-02 15:35:19 -0300

    deployment working

--- ansible/private/run.sh
+++ ansible/private/run.sh
@@ -14,14 +14,6 @@ image=eubraatmosphere/auditing-server:$1
 sudo docker pull $image
 container_id=`sudo docker run --name auditing-server -idt $image`
 
-DIR_PATH="`dirname \"$0\"`"              # relative
-DIR_PATH="`( cd \"$DIR_PATH\" && pwd )`"  # absolutized and normalized
-if [ -z "$DIR_PATH" ] ; then
-  # error; for some reason, the path is not accessible
-  # to the script (e.g. permissions re-evaled after suid)
-  exit 1  # fail
-fi
-
 sudo docker exec $container_id /bin/bash -c "mkdir src/main/resources/private"
-sudo docker cp $DIR_PATH/auditing-server.conf $container_id:/root/auditing-server/src/main/resources/private
+sudo docker cp ./auditing-server.conf $container_id:/root/auditing-server/src/main/resources/private
 sudo docker exec $container_id /bin/bash -c "mvn spring-boot:run -X > log.out 2> log.err" &
