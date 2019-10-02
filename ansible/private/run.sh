07562bff1fdfd5918c8a874080774d13554f50e3
raonismaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2019-10-02 15:55:57 -0300

    fixing sed

--- ansible/private/run.sh
+++ ansible/private/run.sh
@@ -9,9 +9,9 @@ db_url=$host_ip:$db_container_port/$db_name
 db_user=$(grep ^db_user general.conf | awk -F "=" '{print $2}')
 db_password=$(grep ^db_password general.conf | awk -F "=" '{print $2}')
 
-sed -i 's,spring.datasource.url.*,spring.datasource.url=jdbc:postgresql://$db_url,' ./application.properties
-sed -i 's,spring.datasource.username.*,spring.datasource.username=$db_user,' ./application.properties
-sed -i 's,spring.datasource.password.*,spring.datasource.password=$db_password,' ./application.properties
+sed -i "s,spring.datasource.url.*,spring.datasource.url=jdbc:postgresql://$db_url," ./application.properties
+sed -i "s,spring.datasource.username.*,spring.datasource.username=$db_user," ./application.properties
+sed -i "s,spring.datasource.password.*,spring.datasource.password=$db_password," ./application.properties
 
 git clone https://github.com/eubr-atmosphere/auditing-server.git
 mv general.conf auditing-server/database-setup
