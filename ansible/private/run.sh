a2684ba756f797f0c11e29baa2ed40282528cfef
raonismaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2019-10-07 10:57:10 -0300

    some fixes

--- ansible/private/run.sh
+++ ansible/private/run.sh
@@ -12,9 +12,10 @@ db_password=$(grep ^db_password general.conf | awk -F "=" '{print $2}')
 sed -i "s,spring.datasource.url.*,spring.datasource.url=jdbc:postgresql://$db_url," ./application.properties
 sed -i "s,spring.datasource.username.*,spring.datasource.username=$db_user," ./application.properties
 sed -i "s,spring.datasource.password.*,spring.datasource.password=$db_password," ./application.properties
+sed -i "s,server.port.**,server.port=8085," ./application.properties
 
 git clone https://github.com/eubr-atmosphere/auditing-server.git
-mv general.conf auditing-server/database-setup
+cp general.conf auditing-server/database-setup
 cd auditing-server/database-setup
 bash deploy-script.sh
 
@@ -24,7 +25,7 @@ sudo rm -rf auditing-server
 
 image=eubraatmosphere/auditing-server:$1
 sudo docker pull $image
-container_id=`sudo docker run --name auditing-server -idt $image`
+container_id=`sudo docker run --name auditing-server -p 8085:8085 -idt $image`
 
 sudo docker exec $container_id /bin/bash -c "mkdir src/main/resources/private"
 sudo docker cp ./auditing-server.conf $container_id:/root/auditing-server/src/main/resources/private
